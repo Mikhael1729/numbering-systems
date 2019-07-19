420eea3da437022a62eb85a1c4ad863897e70f13
Mikhael Santos Fernández <Mikhael1729@gmail.com>
2019-07-18 19:55:09 -0700

    feat: Added main.py
    It's to test numbering system conversors. Also I add a .gitignore file

---
+++ main.py
@@ -0,0 +1,12 @@
+import sys
+from conversor import convert_to_binary
+
+# Prueba 2
+number = int(sys.argv[1])
+
+print('CONVERSION')
+print('----------')
+print('')
+
+print(convert_to_binary(number))
+
